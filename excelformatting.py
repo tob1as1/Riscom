d64731edf0dde739370ed2f5eb3de63e68e1a4b5
Adrien <uflaw@student.kit.edu>
2025-06-15 18:50:42 +0200

    csv erstellen

--- excelformatting.py
+++ excelformatting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # === 1. Load the Excel file ===
-df = pd.read_excel("Data2.0 (Pumped).xlsx")
+df = pd.read_excel("Excel_version1506.xlsx")
 
 # === 2. Define the text and output fields ===
 text_col = "Unnamed: 0"
